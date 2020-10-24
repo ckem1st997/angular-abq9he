29f363faaa318818c669f686525656d4f97abe44
ckem1st997 <hopdajka97@gmail.com>
2020-10-24 10:30:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-abq9he
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-abq9he)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+import { products } from "../products";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert("The product has been shared!");
+  }
+  price(a) {
+    if (a < 600) {
+      window.alert("Price <600," + a + " !");
+    } else {
+      window.alert("Price >=600," + a + " !");
+    }
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
